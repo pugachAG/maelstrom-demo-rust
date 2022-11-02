14c8667e95ac7bd34dde9f7f6b9190dfb36d6603
Anton Puhach <pugachag@gmail.com>
2022-11-02 17:34:38 +0100

    Add bin directory with run scripts

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod workloads;
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
     if args.len() != 1 {
-        panic!("Expected single argument, got {}", args.len());
+        panic!("Expected single argument, got {:?}", args);
     }
     match args[0].as_str() {
         "echo" => workloads::echo::run(),
